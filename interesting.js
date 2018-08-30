92f2e24361e2535a5b1229785109a99c759892ab
xiaodong ma <mxd_neu@163.com>
2018-08-30 20:46:29 +0800

    link

--- interesting.js
+++ interesting.js
@@ -35,3 +35,21 @@ function Power(base, exponent){
     }
     return base;
 }
+
+//输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，所有的偶数位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+
+function reOrderArray(array){
+    // write code here
+    var oddArr = [],
+        evenArr = [],
+        totalArr = [];
+    for(let i in array){
+        if(array[i]%2 == 0){
+            oddArr.push(array[i]);
+        }else{
+            evenArr.push(array[i]);
+        }    
+    }
+    totalArr = evenArr.concat(oddArr);
+    return totalArr;
+}
--- linkList.js
+++ linkList.js
@@ -14,3 +14,23 @@ function printListFromTailToHead(head)
     }
     return arr;
 }
+
+//输入一个链表，输出该链表中倒数第k个结点。
+
+/*function ListNode(x){
+    this.val = x;
+    this.next = null;
+}*/
+function FindKthToTail(head, k){
+    // write code here
+    if(head == null){
+        return false
+    }
+    var currentNode = head;
+    var arr = [];
+    while(currentNode != null){
+        arr.push(currentNode);
+        currentNode = currentNode.next;
+    }
+    return arr[arr.length -k];
+}
