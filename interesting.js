c61c5714990b5fb2a96f8c46f156455abe110e7b
xiaodong ma <mxd_neu@163.com>
2020-06-28 00:07:15 +0800

    矩形覆盖

--- interesting.js
+++ interesting.js
@@ -119,7 +119,7 @@ function FindGreatestSumOfSubArray(array)
         }
     }
     Sum(array);
-    var result = result.sort(function(a,b){
+    result = result.sort(function(a,b){
         return a -b;
     })
     return result[result.length-1];
@@ -127,4 +127,19 @@ function FindGreatestSumOfSubArray(array)
 
 
 
+// 矩形方块我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
 
+function rectCover(number){
+    // write code here
+    var result = 0;
+    if(number === 0){
+        result = 0;
+    }else if(number === 1){
+        result = 1;
+    }else if(number === 2){
+        result = 2;
+    }else{
+        result =  rectCover(number-1) + rectCover(number-2);
+    }
+    return result;
+}
