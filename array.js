581d021c2d33dcad7da7301eefb1fd4ff715a313
xiaodong ma <mxd_neu@163.com>
2020-07-05 00:53:45 +0800

    只出现一次的数字js简便写法lastIndexOf

--- array.js
+++ array.js
@@ -96,3 +96,16 @@ function FindNumsAppearOnce(array) {
     }
     return array;
 }
+
+function FindNumsAppearOnce1(array)
+{
+    // write code here
+    // return list, 比如[a,b]，其中ab是出现一次的两个数字
+    var arr = [];
+    for(var i = 0; i < array.length; i++) {
+        if(array.indexOf(array[i]) === array.lastIndexOf(array[i])) {
+            arr.push(array[i]);
+        }
+    }
+    return arr;
+}
