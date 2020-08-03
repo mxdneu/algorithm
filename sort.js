3906d9c62d49dc2bdb21de9e70c60df25e89c38e
xiaodong ma <mxd_neu@163.com>
2020-08-04 01:14:51 +0800

    mergesort

--- sort.js
+++ sort.js
@@ -96,3 +96,34 @@ function quickssort(arr){
 	}
 	return quickssort(left).concat([middle],quickssort(right));
 }
+
+// 归并排序
+function mergeSort(arr) {
+	const len = arr.length;
+	if (len < 2) {
+		return arr;
+	}
+	let middle = Math.floor(len/2);
+	let left = arr.slice(0, middle);
+	let right = arr.slice(middle);
+	return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+	let result = [];
+	while (left.length && right.length) {
+		if (left[0] <= right[0]) {
+			result.push(left.shift());
+		} else {
+			result.push(right.shift());
+		}
+	}
+	while (left.length) {
+		result.push(left.shift());
+	}
+	while (right.length) {
+		result.push(right.shift());
+	}
+	return result;
+}
+
