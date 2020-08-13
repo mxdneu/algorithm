4581240f0a9c6aa832ae8773cbcf78bdaecf744f
xiaodong ma <mxd_neu@163.com>
2020-08-14 00:48:24 +0800

    最大公共前缀

--- string.js
+++ string.js
@@ -31,3 +31,20 @@ function LeftRotateString(str, n){
     n=n%str.length;
     return str.substr(n)+str.substr(0,n);   
 }
+
+// ['asd', 'asdasdasd', 'asdasd'], 求最大公共前缀 longest prefix
+var longestCommonPrefix = function(strs) {
+  if (strs === null || strs.length === 0) return "";
+  if(strs.length === 1) return strs[0]
+  let min = 0, max = 0
+  for(let i = 1; i < strs.length; i++) {
+      if(strs[min] > strs[i]) min = i
+      if(strs[max] < strs[i]) max = i
+  }
+  for(let j = 0; j < strs[min].length; j++) {
+      if(strs[min].charAt(j) !== strs[max].charAt(j)) {
+          return strs[min].substring(0, j)
+      }
+  }
+  return strs[min]
+};
